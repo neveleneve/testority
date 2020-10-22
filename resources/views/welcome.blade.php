d9d4ad315b93e18f107c6b1921b6def5469eda60
neveleneve <pandupandu813@gmail.com>
2020-10-22 14:41:56 +0700

    new commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Mohammad Farid Hasymi</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('asset/assets/img/favicon.ico')}}" />
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"
+        type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <link href="{{asset('asset/css/styles.css')}}" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top" id="sideNav">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            <span class="d-block d-lg-none">Mohammad Farid Hasymi</span>
+            <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2"
+                    src="{{asset('asset/assets/img/profile.jpg')}}" alt="" /></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+                class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Tentang</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Pendidikan</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skill</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Minat</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Penghargaan</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#karya">Karya</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container-fluid p-0">
+        <section class="resume-section" id="about">
+            <div class="resume-section-content">
+                <h1 class="mb-0">
+                    Mohammad Farid
+                    <span class="text-info">Hasymi</span>
+                </h1>
+                <div class="subheading">
+                    Jalan Kota Piring · Kecamatan Tanjungpinang Timur · Kota Tanjungpinang · 29123 · (62) 822-8378-4873
+                </div>
+                <div class="subheading mb-5">
+                    <a class="text-info" href="mailto:frdhsym@gmail.com">
+                        frdhsym@gmail.com
+                    </a>
+                </div>
+                <p class="lead mb-5">
+                    I am experienced in leveraging agile frameworks to provide a robust synopsis for
+                    high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to
+                    further the overall value proposition.
+                </p>
+                <div class="social-icons">
+                    <a class="social-icon bg-info" href="//api.whatsapp.com/send?phone=6282283784873"
+                        target="__blank"><i class="fab fa-whatsapp"></i></a>
+                    <a class="social-icon bg-info" href="//t.me/neveleneves" target="__blank"><i
+                            class="fab fa-telegram"></i></a>
+                    <a class="social-icon bg-info" href="//github.com/neveleneve" target="__blank"><i
+                            class="fab fa-github"></i></a>
+                    <a class="social-icon bg-info" href="//instagram.com/frdhsym" target="__blank"><i
+                            class="fab fa-instagram"></i></a>
+                    <a class="social-icon bg-info" href="//twitter.com/mamank_says" target="__blank"><i
+                            class="fab fa-twitter"></i></a>
+                    <a class="social-icon bg-info" href="//facebook.com/pandumfh" target="__blank"><i
+                            class="fab fa-facebook-f"></i></a>
+                    <a class="social-icon bg-info" href="//steamcommunity.com/id/vdpandu/" target="__blank"><i
+                            class="fab fa-steam"></i></a>
+                    <a class="social-icon bg-info" href="//discord.gg/XXeaD7" target="__blank"><i
+                            class="fab fa-discord"></i></a>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <section class="resume-section" id="education">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Pendidikan</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Sekolah Tinggi Teknologi Indonesia Tanjungpinang</h3>
+                        <div class="subheading mb-3">Sarjana Komputer</div>
+                        <div>Teknik Informatika</div>
+                        <p>IPK: 3.30</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Agustus 2016 - Sekarang</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">SMA Negeri 1 Tanjungpinang</h3>
+                        <div class="subheading mb-3">Ilmu Pengetahuan Alam</div>
+                        <p>Akselerasi</p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Agustus 2012 - Mei 2014</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">MTsN Madani Bintan</h3>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Agustus 2009 - Mei 2012</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">SD Negeri 002 Bukit Bestari</h3>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Agustus 2003 - Mei 2009</span></div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0"/>
+        <section class="resume-section" id="skills">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Skills</h2>
+                <div class="subheading mb-3">Bahasa Pemrograman & Framework</div>
+                <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-laravel"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-vuejs"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                </ul>
+                <div class="subheading mb-3">Sistem Kerja</div>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Mengutamakan mobilitas dengan desain yang responsif
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Pengetesan multi peramban
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Manajemen tim
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Pengembangan yang tangkas dan dikerjakan tim
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <hr class="m-0"/>
+        <section class="resume-section" id="interests">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Minat</h2>
+                <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid
+                    skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking,
+                    free climbing, and kayaking.</p>
+                <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television
+                    shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest
+                    technology advancements in the front-end web development world.</p>
+            </div>
+        </section>
+        <hr class="m-0"/>
+        <section class="resume-section" id="awards">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Penghargaan</h2>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        Juara 2 Lomba Pidato Bahasa Arab Kementerian Agama Kabupaten Bintan 2011
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                        Juara 2 Lomba Membaca Kitab Fathul Qarib Musabaqoh Qiroatul Kutub 2010
+                    </li>
+                </ul>
+                <h2 class="mb-5 mt-5">Sertifikat</h2>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-certificate text-warning"></i></span>
+                        NGOBAR (Ngoding Bareng) STT Indonesia Tanjungpinang 2019
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-certificate text-warning"></i></span>
+                        Wakil Presiden Mahasiswa STT Indonesia Tanjungpinang Periode 2019-2020
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-certificate text-warning"></i></span>
+                        Pembelajaran HTML Dasar STT Indonesia Tanjungpinang 2018
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-certificate text-warning"></i></span>
+                        Pembelajaran Visual Basic Dasar STT Indonesia Tanjungpinang 2018
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-certificate text-warning"></i></span>
+                        Pembelajaran Desain Grafis (GIMP) Dasar STT Indonesia Tanjungpinang 2018
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-certificate text-warning"></i></span>
+                        Pembelajaran Desain Grafis (AutoCAD) Dasar STT Indonesia Tanjungpinang 2018
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-certificate text-warning"></i></span>
+                        Sekretaris Badan Eksekutif Mahasiswa STT Indonesia Tanjungpinang Periode 2018-2019
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-certificate text-warning"></i></span>
+                        Ketua OSIS MTsN Madani Bintan Periode 2010-2011
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <hr class="m-0" />
+        <section class="resume-section" id="karya">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Hasil Karya Website</h2>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-minus"></i></span>
+                        <a class="text-info" href="//termosin.store" target="__blank">
+                            Termosin Store
+                        </a>
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-minus"></i></span>
+                        <a class="text-info" href="//sipegas.testority.xyz" target="__blank">
+                            Sistem Informasi Pembelian Gas
+                        </a>
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-minus"></i></span>
+                        <a class="text-info" href="//evoting.testority.xyz" target="__blank">
+                            Aplikasi Pemilihan Raya Mahasiswa STT Indonesia Tanjungpinang
+                        </a>
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-minus"></i></span>
+                        <a class="text-info" href="//sipentor.testority.xyz" target="__blank">
+                            Sistem Informasi Pengolahan Inventory
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="{{asset('asset/js/scripts.js')}}"></script>
+</body>
+
+</html>
